e0c420298424c93c045b53675d8029ae24f61000
Azharuddin Ansari <115046468+Azharuddin91284@users.noreply.github.com>
2023-09-16 11:25:54 +0000

    arrayconcept

---
+++ .devcontainer/array.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int n=3;
+    int p[20]={1,4,6,9};
+    // using indexing to check the value of array element
+    cout<<p[1]<<endl;
+    cout<<p[0]<<endl;
+    cout<<"PRint the array here "<<endl;
+    
+
+    for(int i=0;i<=n;i++){
+        cout<<" "<<p[i];
+    }
+}
